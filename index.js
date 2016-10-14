e4b5d485dd6f04e726360535a18c815bfa5f6e8e
aydan brown <aydan brown>
2016-10-14 15:47:14 +1300

    Begun restructuring project

--- index.js
+++ index.js
@@ -1,43 +1,25 @@
-/**
- * This sample demonstrates a simple skill built with the Amazon Alexa Skills Kit.
- * The Intent Schema, Custom Slots, and Sample Utterances for this skill, as well as
- * testing instructions are located at http://amzn.to/1LzFrj6
- *
- * For additional samples, visit the Alexa Skills Kit Getting Started guide at
- * http://amzn.to/1LGWsLG
- */
+const alexa = require('alexa');
 
-// Route the incoming request based on type (LaunchRequest, IntentRequest,
-// etc.) The JSON body of the request is provided in the event parameter.
 exports.handler = function (event, context) {
   try {
-    console.log("event.session.application.applicationId=" + event.session.application.applicationId);
+    console.log('Requesting skill with Application ID: ' + event.session.application.applicationId);
 
-    console.log(JSON.stringify(event, true, 3));
-
-    /**
-     * Uncomment this if statement and populate with your skill's application ID to
-     * prevent someone else from configuring a skill that sends requests to this function.
-     */
-    /*
-    if (event.session.application.applicationId !== "amzn1.echo-sdk-ams.app.[unique-value-here]") {
-         context.fail("Invalid Application ID");
-    }
-    */
-
-    if (event.session.new) {
-      onSessionStarted({requestId: event.request.requestId}, event.session);
+    if(event.session.new) {
+      onSessionStarted({ requestId: event.request.requestId }, event.session);
     }
 
-    if (event.request.type === "LaunchRequest") {
-      onLaunch(event.request, event.session, function(sessionAttributes, speechletResponse) {
+    // Begining session
+    if(event.request.type === "LaunchRequest") {
+      alexa.intent.Launch(event.request, event.session, function(sessionAttributes, speechletResponse) {
         context.succeed(buildResponse(sessionAttributes, speechletResponse));
       });
-    } else if (event.request.type === "IntentRequest") {
+    // During session
+    } else if(event.request.type === "IntentRequest") {
       onIntent(event.request, event.session, function(sessionAttributes, speechletResponse) {
         context.succeed(buildResponse(sessionAttributes, speechletResponse));
       });
-    } else if (event.request.type === "SessionEndedRequest") {
+    // Ending session
+    } else if(event.request.type === "SessionEndedRequest") {
       onSessionEnded(event.request, event.session);
       context.succeed();
     }
@@ -46,90 +28,42 @@ exports.handler = function (event, context) {
   }
 };
 
-/**
- * Called when the session starts.
- */
+// Called when the user launches the skill
 function onSessionStarted(sessionStartedRequest, session) {
   console.log(`onSessionStarted requestId=${sessionStartedRequest.requestId}, sessionId=${session.sessionId}`);
+  // TODO: Set default session values
 }
 
-// Called when the user launches the skill without specifying what they want.
-function onLaunch(launchRequest, session, callback) {
-  console.log(`onLaunch requestId=${launchRequest.requestId}, sessionId=${session.sessionId}`);
-  getWelcomeResponse(callback);
-}
-
-// Called when the user specifies an intent for this skill.
+// Called when the user specifies an intent for this skill
 function onIntent(intentRequest, session, callback) {
   console.log(`onIntent requestId=${intentRequest.requestId}, sessionId=${session.sessionId}`);
 
   var intent = intentRequest.intent;
 
-  var Intent = require('intent');
+  console.log('Invoking Intent - ' + intent.name);
 
-  console.log(`Invoking Intent - ${intentRequest.intent.name}`);
-
-  // Dispatch to your skill's intent handlers
-  switch(intentRequest.intent.name) {
-    case 'SelectChannel': Intent.selectChannel(intent, session, callback); break;
-    case 'MessageChannel': Intent.sendMessageChannel(intent, session, callback); break;
-    case 'ReadMessageChannel': Intent.readMessageChannel(intent, session, callback); break;
-    case 'ListMessagesChannel': Intent.listMessagesChannel(intent, session, callback); break;
-    case 'AMAZON.HelpIntent': Intent.help(intent, session, callback); break;
-    case 'AMAZON.StopIntent': handleSessionEndRequest(callback); break;
-    case 'AMAZON.CancelIntent': handleSessionEndRequest(callback); break;
-    default: throw 'Invalid intent';
+  // Dispatch the intent handler
+  switch(intent.name) {
+    // Amazon intent mapping
+    case 'AMAZON.HelpIntent': alexa.intent.Help(intent, session, callback); break;
+    case 'AMAZON.StopIntent': alexa.intent.End(intent, session, callback); break;
+    case 'AMAZON.CancelIntent': alexa.intent.End(intent, session, callback); break;
+    // Cutom intent handling
+    default:
+      if(alexa.intent[intent.name]) {
+        alexa.intent[intent.name]()
+      } else {
+        // Called if there is no match
+        alexa.intent.None(intent, session, callback);
+      }
   }
 }
 
 
-// Called when the user ends the session.
+// Called when the user ends the session
 function onSessionEnded(sessionEndedRequest, session) {
   console.log(`onSessionEnded requestId=${sessionEndedRequest.requestId}, sessionId=${session.sessionId}`);
-  // Add cleanup logic here
-}
-
-// --------------- Functions that control the skill's behavior -----------------------
-
-function getWelcomeResponse(callback) {
-  // If we wanted to initialize the session to have some attributes we could add those here.
-  var sessionAttributes = { channel: 'C2MMCN5EH', name: 'Alexa' };
-  var cardTitle = "Welcome";
-  var speechOutput = "Welcome to Slack";
-  var repromptText = "If you are unsure, you can ask for help";
-  var shouldEndSession = false;
-
-  callback(sessionAttributes, buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
-}
-
-function handleSessionEndRequest(callback) {
-  var cardTitle = "Session Ended";
-  var speechOutput = "Have a nice day";
-  // Setting this to true ends the session and exits the skill.
-  var shouldEndSession = true;
-
-  callback({}, buildSpeechletResponse(cardTitle, speechOutput, null, shouldEndSession));
-}
-
-function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
-  return {
-    outputSpeech: {
-      type: "PlainText",
-      text: output
-    },
-    card: {
-      type: "Simple",
-      title: "SessionSpeechlet - " + title,
-      content: "SessionSpeechlet - " + output
-    },
-    reprompt: {
-      outputSpeech: {
-        type: "PlainText",
-        text: repromptText
-      }
-    },
-    shouldEndSession: shouldEndSession
-  };
+  // TODO: Cleanup
 }
 
 function buildResponse(sessionAttributes, speechletResponse) {
